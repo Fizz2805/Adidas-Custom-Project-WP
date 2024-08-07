5304dfab3c6b6a4239e50cfd2572e8cd4b166be9
unknown <fizzajavaid765.com>
2024-08-07 15:40:38 -0700

    first commit

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -19,14 +19,4 @@
 //         primaryMenu.classList.toggle('open');
 //     });
 // });
-// document.addEventListener('DOMContentLoaded', function() {
-//     const hamburger = document.querySelector('.hamburger');
-//     const primaryMenu = document.querySelector('.primary-menu');
-
-//     if (hamburger && primaryMenu) {
-//         hamburger.addEventListener('click', function() {
-//             console.log('Hamburger clicked'); // This should log when you click the hamburger
-//             primaryMenu.classList.toggle('open'); /* Toggle the menu visibility */
-//         });
-//     }
-// });
+// meu
